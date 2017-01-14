8deb7a2a563467511cd92de5b4fd42ded9826409
Mibert <Razo412@gmail.com>
2017-01-14 17:10:32 +0200

    Saving the text if the article into .txt file

--- app/News.php
+++ app/News.php
@@ -3,6 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * Class News
+ * @package App
+ * @property id
+ * @property $text
+ * @property $title
+ * @property $author
+ * @property $categories
+ */
 
 class News extends Model
 {
@@ -12,6 +23,23 @@ class News extends Model
 	 * @var string
 	 */
 	protected $table = 'news';
+
+
+	/**
+	 * Disk-driver, that laravel used to create and store files created by this model.
+	 * For now only local disk is needed.
+	 *
+	 * @var string
+	 */
+	protected $defaultDisk = 'local';
+
+
+	/**
+	 * This is default directory for storing files created by this model.
+	 *
+	 * @var string
+	 */
+	protected $defaultDir = 'news';
 
 
 	/**
@@ -44,6 +72,14 @@ class News extends Model
 	);
 
 
+	/**
+	 * Better to save all categories in only one field in DB,
+	 * so all categories that have been choosen - transformed into a string from the array.
+	 * Each category in this string separated by ', '.
+	 *
+	 * @param array $data
+	 * @return string
+	 */
 	public static function categoriesToStr($data) {
 		$categories = [];
 		foreach ($data as $key=>$value) {
@@ -53,8 +89,13 @@ class News extends Model
 		}
 		return implode(', ', $categories);
 	}
-	
-	
+
+
+	/**
+	 * Array with static rules for validation of user input
+	 *
+	 * @return array
+	 */
 	public static function getValidationRules()
 	{
 		// проинициализируем массив статичеческими правилами
@@ -65,5 +106,32 @@ class News extends Model
 		);
 
 		return $validation;
+	}
+
+
+	/**
+	 * Save the text of the article in .txt file.
+	 * Return a boolean if there is a need only to edit an existing article or
+	 * return a path to the created file if it's the new one (for saving in DB later).
+	 *
+	 * @param string $text
+	 * @return bool|string
+	 */
+	public function saveInTxtFile($text)
+	{
+		if(Storage::disk($this->defaultDisk)) {
+			//For editing the text of the existing article - return bool
+			if($this->text) {
+				Storage::put($this->text, $text);
+				return true;
+			}
+
+			//For creating the .txt file for the new article - return a path to this file.
+			$dir = $this->defaultDir . DIRECTORY_SEPARATOR . str_replace(' ', '_', strtolower($this->title));
+			$file_path = $dir . DIRECTORY_SEPARATOR . 'text.txt';
+			Storage::put($file_path, $text);
+			
+			return $file_path;
+		}
 	}
 }
